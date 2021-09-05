26d33f6c8409ca085013972d6ac5d8c637af973d
Ignashev Roman <ignashevroman99@gmail.com>
2021-09-05 12:06:37 +0300

    Add UsesUuid trait for orders. Add Order model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\UsesUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+    use UsesUuid;
+
+    protected $fillable = [
+        'quantity_of_completed',
+        'quantity',
+        'charge',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function profile(): BelongsTo
+    {
+        return $this->belongsTo(Profile::class, 'profile_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class, 'service_id', 'service');
+    }
+}
